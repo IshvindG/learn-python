cd4458044607d37356c3ce3a9ed7198aac12d510
IshvindG <ishvindgrewal7@gmail.com>
2024-08-22 13:22:04 +0100

    Update Salshipping.py

--- 2-control-flow/sals-shipping/shipping.py
+++ 2-control-flow/sals-shipping/shipping.py
@@ -1,36 +1,39 @@
-# Sal's Shipping
-# Sonny Li
+weight = 41.5
 
-weight = 80
 
-# Ground Shipping 🚚
+flat_charge_premium = 125
+flat_charge_ground = 20
+price_per_pound = 0
+cost_ground = 0
+#Ground Shipping 
+if weight <= 2:
+  price_per_pound += 1.5*weight
+  cost_ground = flat_charge_ground+price_per_pound
+elif 2 < weight <= 6:
+  price_per_pound += 3*weight
+  cost_ground = flat_charge_ground + price_per_pound
+elif 6 < weight <= 10:
+  price_per_pound += 4*weight
+  cost_ground = flat_charge_ground + price_per_pound
+elif weight > 10:
+  price_per_pound += 4.75*weight
+  cost_ground = flat_charge_ground + price_per_pound
+
+print('Flat charge for ground shipping premium is {}'.format(flat_charge_premium))
+print('Flat charge for standard ground shipping is {}'.format(flat_charge_ground))
+
+
+#Drone Shipping
 
 if weight <= 2:
-  cost_ground = weight * 1.5 + 20
-elif weight <= 6:
-  cost_ground = weight * 3.00 + 20
-elif weight <= 10:
-  cost_ground = weight * 4.00 + 20
-else:
-  cost_ground = weight * 4.75 + 20
-
-print("Ground Shipping $", cost_ground)
-      
-# Ground Shipping Premimum 🚚💨
-
-cost_ground_premium = 125.00
-
-print("Ground Shipping Premimium $", cost_ground_premium)
-
-# Drone Shipping 🛸
-
-if weight <= 2:
-  cost_drone = weight * 4.5
-elif weight <= 6:
-  cost_drone = weight * 9.00
-elif weight <= 10:
-  cost_drone = weight * 12.00
-else:
-  cost_drone = weight * 14.25
-
-print("Drone Shipping: $", cost_drone)
+  cost_drone = 4.50 * weight
+elif 2 < weight <= 6:
+  cost_drone = 9 * weight
+elif 6 < weight <= 10:
+  cost_drone = 12 * weight
+elif weight > 10:
+  cost_drone = 14.25 * weight 
+print('')
+print('Your total is for standard ground shipping is ' + str(cost_ground))
+print('Your total is drone shipping is ' + str(cost_drone))
+print('Your total is ' + str(flat_charge_premium))
